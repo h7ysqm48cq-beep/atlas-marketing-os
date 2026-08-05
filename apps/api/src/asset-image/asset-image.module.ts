bab32a4170ffaa25ef4977717bf9fec9fdc30f4d
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-06 05:08:46 +0800

    Register logo engine services in asset image module

--- apps/api/src/asset-image/asset-image.module.ts
+++ apps/api/src/asset-image/asset-image.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BrandsModule } from '../brands/brands.module';
+import {
+  LogoLayoutService,
+  LogoOverlayService,
+  SafeAreaService,
+} from '../image/logo';
 import { StorageModule } from '../storage/storage.module';
 import { AssetImageBackgroundJobService } from './asset-image-background-job.service';
 import { AssetImageController } from './asset-image.controller';
@@ -8,7 +13,19 @@ import { AssetImageService } from './asset-image.service';
 @Module({
   imports: [BrandsModule, StorageModule],
   controllers: [AssetImageController],
-  providers: [AssetImageService, AssetImageBackgroundJobService],
-  exports: [AssetImageService, AssetImageBackgroundJobService],
+  providers: [
+    AssetImageService,
+    AssetImageBackgroundJobService,
+    LogoOverlayService,
+    LogoLayoutService,
+    SafeAreaService,
+  ],
+  exports: [
+    AssetImageService,
+    AssetImageBackgroundJobService,
+    LogoOverlayService,
+    LogoLayoutService,
+    SafeAreaService,
+  ],
 })
 export class AssetImageModule {}
