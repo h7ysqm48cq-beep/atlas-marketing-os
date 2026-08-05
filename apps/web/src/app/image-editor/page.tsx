3420cbdb5a7bf52a1fc2f5cfb7607a368bf41c19
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-06 06:40:10 +0800

    Add image editor page

---
+++ apps/web/src/app/image-editor/page.tsx
@@ -0,0 +1,10 @@
+import { AppLayout } from "@/components/AppLayout";
+import { ImageBrandEditor } from "@/components/ImageBrandEditor";
+
+export default function ImageEditorPage() {
+  return (
+    <AppLayout>
+      <ImageBrandEditor />
+    </AppLayout>
+  );
+}
