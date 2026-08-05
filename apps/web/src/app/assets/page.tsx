645a4e9a21afee27b0570f305abfd9d37dc90f97
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-06 07:15:43 +0800

    Load Asset Library mobile visual refresh

--- apps/web/src/app/assets/page.tsx
+++ apps/web/src/app/assets/page.tsx
@@ -1,5 +1,6 @@
 import { AssetLibrary } from "@/components/AssetLibrary";
 import { AppLayout } from "@/components/AppLayout";
+import "./asset-library-mobile.css";
 
 export default function AssetsPage() {
   return (
