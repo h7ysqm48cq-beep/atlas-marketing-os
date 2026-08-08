f927eb02612e36c90d167c1f4614074dbab43cee
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 13:58:55 +0800

    feat: expose sports news settings API

---
+++ apps/api/src/automation/sports-news-settings.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { SportsNewsSettingsService, UpdateSportsNewsSettingsInput } from './sports-news-settings.service';
+
+@Controller('sports-news')
+export class SportsNewsSettingsController {
+  constructor(private readonly settings: SportsNewsSettingsService) {}
+
+  @Get('settings')
+  getSettings() { return this.settings.get(); }
+
+  @Patch('settings')
+  updateSettings(@Body() body: UpdateSportsNewsSettingsInput) { return this.settings.update(body); }
+
+  @Get('channels')
+  channels() { return this.settings.channels(); }
+}
