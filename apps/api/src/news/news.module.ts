dba6b74a81d206d6b777881624465ace6e46cef6
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 15:20:39 +0800

    feat: register sports news run history

--- apps/api/src/news/news.module.ts
+++ apps/api/src/news/news.module.ts
@@ -4,11 +4,12 @@ import { ImageModule } from '../image/image.module';
 import { SportsNewsGeneratorService } from './sports-news-generator.service';
 import { SportsNewsImageService } from './sports-news-image.service';
 import { SportsNewsPublishService } from './sports-news-publish.service';
+import { SportsNewsRunHistoryService } from './sports-news-run-history.service';
 import { SportsNewsSourceService } from './sports-news-source.service';
 
 @Module({
   imports: [AiProviderModule, ImageModule],
-  providers: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService],
-  exports: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService],
+  providers: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService, SportsNewsRunHistoryService],
+  exports: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService, SportsNewsRunHistoryService],
 })
 export class NewsModule {}
