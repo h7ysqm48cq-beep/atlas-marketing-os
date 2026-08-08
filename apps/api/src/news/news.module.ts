3ed4dbb917419a5c06074c51c0029164fcb67992
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 14:40:33 +0800

    feat: register sports news image generator

--- apps/api/src/news/news.module.ts
+++ apps/api/src/news/news.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AiProviderModule } from '../ai-provider/ai-provider.module';
+import { ImageModule } from '../image/image.module';
 import { SportsNewsGeneratorService } from './sports-news-generator.service';
+import { SportsNewsImageService } from './sports-news-image.service';
 import { SportsNewsSourceService } from './sports-news-source.service';
 
 @Module({
-  imports: [AiProviderModule],
-  providers: [SportsNewsSourceService, SportsNewsGeneratorService],
-  exports: [SportsNewsSourceService, SportsNewsGeneratorService],
+  imports: [AiProviderModule, ImageModule],
+  providers: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService],
+  exports: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService],
 })
 export class NewsModule {}
