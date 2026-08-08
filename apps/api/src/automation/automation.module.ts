0472124804a58b16ad8a5ca1e19b6940317e9924
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 14:09:46 +0800

    feat: register sports news source validator

--- apps/api/src/automation/automation.module.ts
+++ apps/api/src/automation/automation.module.ts
@@ -7,23 +7,11 @@ import { PublisherService } from './publisher.service';
 import { AutomationSchedulerService } from './automation-scheduler.service';
 import { SportsNewsSettingsController } from './sports-news-settings.controller';
 import { SportsNewsSettingsService } from './sports-news-settings.service';
+import { SportsNewsSourceValidatorService } from './sports-news-source-validator.service';
 
 @Module({
   controllers: [AutomationController, SportsNewsSettingsController],
-  providers: [
-    AutomationSchedulerService,
-    AutomationService,
-    SportsNewsSettingsService,
-    TelegramConnectorService,
-    FacebookConnectorService,
-    PublisherService,
-  ],
-  exports: [
-    AutomationService,
-    SportsNewsSettingsService,
-    TelegramConnectorService,
-    FacebookConnectorService,
-    PublisherService,
-  ],
+  providers: [AutomationSchedulerService, AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, TelegramConnectorService, FacebookConnectorService, PublisherService],
+  exports: [AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, TelegramConnectorService, FacebookConnectorService, PublisherService],
 })
 export class AutomationModule {}
