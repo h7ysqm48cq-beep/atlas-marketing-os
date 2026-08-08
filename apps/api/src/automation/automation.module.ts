a86815c7a4db2010d2403e128bb23ebdd1804e8d
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 14:18:13 +0800

    feat: register sports news runner

--- apps/api/src/automation/automation.module.ts
+++ apps/api/src/automation/automation.module.ts
@@ -8,10 +8,11 @@ import { AutomationSchedulerService } from './automation-scheduler.service';
 import { SportsNewsSettingsController } from './sports-news-settings.controller';
 import { SportsNewsSettingsService } from './sports-news-settings.service';
 import { SportsNewsSourceValidatorService } from './sports-news-source-validator.service';
+import { SportsNewsRunnerService } from './sports-news-runner.service';
 
 @Module({
   controllers: [AutomationController, SportsNewsSettingsController],
-  providers: [AutomationSchedulerService, AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, TelegramConnectorService, FacebookConnectorService, PublisherService],
-  exports: [AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, TelegramConnectorService, FacebookConnectorService, PublisherService],
+  providers: [AutomationSchedulerService, AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, SportsNewsRunnerService, TelegramConnectorService, FacebookConnectorService, PublisherService],
+  exports: [AutomationService, SportsNewsSettingsService, SportsNewsSourceValidatorService, SportsNewsRunnerService, TelegramConnectorService, FacebookConnectorService, PublisherService],
 })
 export class AutomationModule {}
