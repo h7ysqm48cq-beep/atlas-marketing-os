3227bcd64ce4e059303dd9a82a5fd3444eef79f8
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-08 14:45:07 +0800

    feat: register sports news publisher

--- apps/api/src/news/news.module.ts
+++ apps/api/src/news/news.module.ts
@@ -3,11 +3,12 @@ import { AiProviderModule } from '../ai-provider/ai-provider.module';
 import { ImageModule } from '../image/image.module';
 import { SportsNewsGeneratorService } from './sports-news-generator.service';
 import { SportsNewsImageService } from './sports-news-image.service';
+import { SportsNewsPublishService } from './sports-news-publish.service';
 import { SportsNewsSourceService } from './sports-news-source.service';
 
 @Module({
   imports: [AiProviderModule, ImageModule],
-  providers: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService],
-  exports: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService],
+  providers: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService],
+  exports: [SportsNewsSourceService, SportsNewsGeneratorService, SportsNewsImageService, SportsNewsPublishService],
 })
 export class NewsModule {}
