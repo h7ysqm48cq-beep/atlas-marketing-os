bf1164b84cc8a911fdb90ceca799470e739ae71a
Loh <h7ysqm48cq@privaterelay.appleid.com>
2026-08-22 01:14:02 +0800

    fix system health browser worker mapping and cleanup backups

---
+++ apps/web/src/app/system-health/page.tsx
@@ -0,0 +1,10 @@
+import { AppLayout } from "@/components/AppLayout";
+import { SystemHealthDashboard } from "@/components/system-health/SystemHealthDashboard";
+
+export default function SystemHealthPage() {
+  return (
+    <AppLayout>
+      <SystemHealthDashboard />
+    </AppLayout>
+  );
+}
