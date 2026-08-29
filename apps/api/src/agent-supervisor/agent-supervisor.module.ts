bf14de253e75c8b849fd4bae8ab4d488e6ef0fec
h7ysqm48cq-beep <h7ysqm48cq@privaterelay.appleid.com>
2026-08-30 03:33:21 +0800

    refactor: wire supervisor memory stores

--- apps/api/src/agent-supervisor/agent-supervisor.module.ts
+++ apps/api/src/agent-supervisor/agent-supervisor.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AgentSupervisorController } from './agent-supervisor.controller';
 import { AgentSupervisorService } from './agent-supervisor.service';
+import {
+  FILE_OWNERSHIP_STORE,
+} from './stores/file-ownership.store';
+import { MemoryFileOwnershipStore } from './stores/memory-file-ownership.store';
+import { MemorySupervisorTaskStore } from './stores/memory-supervisor-task.store';
+import {
+  SUPERVISOR_TASK_STORE,
+} from './stores/supervisor-task.store';
 
 @Module({
   controllers: [AgentSupervisorController],
-  providers: [AgentSupervisorService],
-  exports: [AgentSupervisorService],
+  providers: [
+    AgentSupervisorService,
+    MemorySupervisorTaskStore,
+    MemoryFileOwnershipStore,
+    {
+      provide: SUPERVISOR_TASK_STORE,
+      useExisting: MemorySupervisorTaskStore,
+    },
+    {
+      provide: FILE_OWNERSHIP_STORE,
+      useExisting: MemoryFileOwnershipStore,
+    },
+  ],
+  exports: [
+    AgentSupervisorService,
+    SUPERVISOR_TASK_STORE,
+    FILE_OWNERSHIP_STORE,
+  ],
 })
 export class AgentSupervisorModule {}
